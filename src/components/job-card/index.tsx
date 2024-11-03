af0eccd92b7df2edd3e7385e5b799f3115504e93
AhmedAbdullahFahim <ahmed.abdallah5022@gmail.com>
2024-11-03 23:14:04 +0200

    feat: job card component

--- src/components/job-card/index.tsx
+++ src/components/job-card/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Job } from '../../../types'
+import Tag from '../tag'
 import styles from './index.module.scss'
 
 interface Props {
@@ -7,7 +9,20 @@ interface Props {
 }
 
 const JobCard: React.FC<Props> = ({ job }: Props) => {
-  return <div className={styles.card}>{job.attributes.title}</div>
+  return (
+    <div className={styles.card}>
+      <h6>{job.attributes.title}</h6>
+      <div className={styles.skillsWrapper}>
+        <p>Related Skills:</p>
+        <div className={styles.skills}>
+          {job.relationships.skills.map((skill, index) => (
+            <Tag key={`${skill.id} ${index}`} id={skill.id} />
+          ))}
+        </div>
+      </div>
+      <Link to='/jobs/search'>View job details</Link>
+    </div>
+  )
 }
 
 export default JobCard
